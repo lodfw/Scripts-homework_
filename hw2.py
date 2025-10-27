a44c238de0787fae68cea523da292f7ed480232c
lodfw <135309628+lodfw@users.noreply.github.com>
2025-10-27 13:21:48 +0800

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -18,14 +18,14 @@ for article in bookname_articles:
     title = a_tag.get("title", "").strip()#從 <a> 標籤裡取出 title 屬性 #strip() 去除前後空白
  
 
-    price_tag = article.find("p", class_="price_color")
+    price_tag = article.find("p", class_="price_color")#使用 BeautifulSoup 的 find 方法，在 article 這個 HTML 區塊中，尋找第一個 <p> 標籤，且它的 class 屬性包含 ""price_color"。
     price = price_tag.get_text().strip()
    
 
-    rating_tag = article.find("p", class_="star-rating")
+    rating_tag = article.find("p", class_="star-rating")#使用 BeautifulSoup 的 find 方法，在 article 這個 HTML 區塊中，尋找第一個 <p> 標籤，且它的 class 屬性包含 "star-rating"。
     rating_classes = rating_tag.get("class", [])  # 取得 class 屬性（是個 list）
     # 移除 "star-rating"，留下星等
-    rating = next((c for c in rating_classes if c != "star-rating"), "No rating")
+    rating = next((c for c in rating_classes if c != "star-rating"), "No rating")#從 rating_classes 裡面，找出第一個 不是 "star-rating" 的 class，並把它存到 rating。["star-rating", "Three"]
     book = {
         "title": title,
         "price": price,
@@ -35,3 +35,4 @@ for article in bookname_articles:
 #print(json.dumps(books, indent=2, ensure_ascii=False))
 print("書名列表：")
 print(json.dumps(books, indent=2, ensure_ascii=False))
+
